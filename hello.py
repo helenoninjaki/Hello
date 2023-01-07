cfc95409fa41ba9a7b663a5aa2f9f3d1f2ff0a28
helenoninjaki <122110143+helenoninjaki@users.noreply.github.com>
2023-01-07 11:40:39 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+
+from flask import Flask
+ 
+
+app = Flask(__name__)
+ 
+
+@app.route('/')
+
+def hello_world():
+    return 'Hello ninja'
+ 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
